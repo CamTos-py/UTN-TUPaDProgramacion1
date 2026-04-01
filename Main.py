40a09c472f28ea50ce2eaf2671e8b0cf3dfe2abb
CamTos-py <camilatosti1@gmail.com>
2026-04-01 17:29:57 -0300

    Subo pruebas de Unidad 4

---
+++ Main.py
@@ -0,0 +1,6 @@
+print("¡Hola Mundo desde Main!")
+
+numero = int(input("Ingrese un número: "))
+numero2 = int(input("Ingrese otro número: "))
+
+print(f"El resultado de la suma es: {numero + numero2}")
